a6421c01dca075ec09d5f4d2a011bded22622074
Brendan Norris <brendan.t.norris@gmail.com>
2023-07-14 19:13:01 -0700

    updated title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -263,7 +263,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-6xl">
-            web slinger <br/> space cowboy <br/> c2 irl
+            web slinger <br/> space cowboy <br/> chmp irl 🏎️
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             i&apos;m walker norris, a web slinger based in castle hayne, nc. <br/> i&apos;m into all things react, wild west, and rocket league. <br/> - currently working for Thinkhuman.co.
